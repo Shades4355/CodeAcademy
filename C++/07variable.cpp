cb261dcc2af4e6a71cd10f6775ed4a066c9097fc
Shades <shades4355@gmail.com>
2020-05-24 15:57:36 -0400

    2020-05-24 15:57

---
+++ C++/07variable.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+
+  // Declare a variable
+  int year;
+
+  // Initialize a variable
+  year = 2019;
+
+  // Declare and Initialize on same line
+  int score = 0;
+
+}
