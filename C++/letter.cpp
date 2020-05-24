acc46831961842d683d2a2518ffa4612c05649b1
Shades <shades4355@gmail.com>
2020-05-24 15:25:11 -0400

    letter.cpp finished

--- C++/letter.cpp
+++ C++/letter.cpp
@@ -2,7 +2,11 @@
 
 int main()
 {
+
   std::cout << "May 24, 2020\n\n";
-  std::cout << "Dear Shades,\n";
+  std::cout << "Dear Self,\n";
+  std::cout << "My goal for this week is to make strong headway on C++ and/or Java.\n\n";
+  std::cout << "signed,\n";
+  std::cout << "Shades";
 
 }
