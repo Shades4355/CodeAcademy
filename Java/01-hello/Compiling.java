92e464973e9ace8c808ca51e13db715df1bcc8ff
Shades <shades4355@gmail.com>
2020-05-29 16:22:58 -0400

    Java - Hello World

---
+++ Java/01-hello/Compiling.java
@@ -0,0 +1,23 @@
+/*
+For example, with a file called Plankton.java, we could compile it with the terminal command:
+
+[javac Plankton.java]
+
+A successful compilation produces a .class file: Plankton.class, that we execute with the terminal command:
+
+[java Plankton]
+
+An unsuccessful compilation produces a list of errors. No .class file is made until the errors are corrected and the compile command is run again.
+*/
+
+public class Compiling {
+  public static void main(String[] args) {
+
+    System.out.println("Java is a class-based language.");
+    System.out.println("Java classes have a 'main' method.");
+    System.out.println("Java statements end with a semicolon.")
+
+    System.out.println("Programming is... fun!");
+
+  }
+}
---
+++ Java/01-hello/LanguageFacts.java
@@ -0,0 +1,25 @@
+/*
+Java does not interpret whitespace, the areas of the code without syntax, but humans use whitespace to read code without difficulty.
+
+Java does interpret semicolons. Semicolons are used to mark the end of a statement, one line of code that performs a single task.
+*/
+
+/*
+public class LanguageFacts {
+  public static void main(String[] args) {
+    // Press enter or return on your keyboard after each semicolon!
+
+    System.out.println("Java is a class-based language.");System.out.println("Java classes have a 'main' method.");System.out.println("Java statements end with a semicolon.");
+  }
+}
+*/
+
+public class LanguageFacts {
+  public static void main(String[] args) {
+    System.out.println("Java is a class-based language.");
+    System.out.println("Java classes have a 'main' method.");
+    System.out.println("Java statements end with a semicolon.");
+
+    System.out.println("Programming is fun.");
+  }
+}
