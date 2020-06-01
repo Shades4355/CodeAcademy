e8cde34f8102e545df056f0a9338a298c8753867
Shades <shades4355@gmail.com>
2020-06-01 11:36:13 -0400

    Java - variables (finished) - Mess (all previous types) - BadNames (illegal variable names)

---
+++ Java/02-variables/BadNames.java
@@ -0,0 +1,24 @@
+/*
+public class BadNames {
+	public static void main(String[] args) {
+		String 1stName = "Samira";  // illegal declaration
+    String blah = "Smith";
+    String .com = "samira@google.com";  // illegal declaration
+    int salaryexpectation = 100000;
+    int year_of_birth = 1955;
+
+    System.out.println("The program runs!");
+	}
+}
+*/
+public class BadNames {
+	public static void main(String[] args) {
+		String firstName = "Samira";
+    String blah = "Smith";
+    String email = "samira@google.com";
+    int salaryexpectation = 100000;
+    int year_of_birth = 1955;
+
+    System.out.println("The program runs!");
+	}
+}
