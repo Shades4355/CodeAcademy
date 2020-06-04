f1a616de2812574ed8f65b1b742183aae9d4e68f
Shades <shades4355@gmail.com>
2020-06-03 20:50:54 -0400

    Java - If-Then-Else (lesson)

--- Java/06-flow_controle/Order.java
+++ Java/06-flow_controle/Order.java
@@ -2,11 +2,13 @@ public class Order {
 
   public static void main(String[] args) {
 
-    double itemCost = 30.99;
+    boolean isFilled = false;
 
-    // Write an if-then statement:
-    if (itemCost > 24.00) {
-      System.out.println("High value item!");
+    // Write an if-then-else statement:
+    if (isFilled == true) {
+      System.out.println("Shipping");
+    } else {
+      System.out.println("Order not ready.");
     };
 
   }
