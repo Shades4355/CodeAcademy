ff4ec761e4bdff98d83b508e5490ddd1c60d7b4e
Shades <shades4355@gmail.com>
2020-06-10 16:51:46 -0400

    Java - String Methods, intro

---
+++ Java/11-String_Methods/Intro.java
@@ -0,0 +1,17 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+    String str = "Hello, World!";
+
+    // Examples:
+
+    System.out.println(str.length()); // 13
+
+    System.out.println(str.substring(4)); // "o, World!"
+
+    System.out.println(str.toUpperCase()); // "HELLO, WORLD!"
+
+  }
+
+}
