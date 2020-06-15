f6c941618ed8a94a6c4a9894f90c1a2f58b21e2e
Shades <shades4355@gmail.com>
2020-06-15 17:24:29 -0400

    Java - Syntax Errors

---
+++ Java/13-Bugs/Syntax_Errors.java
@@ -0,0 +1,26 @@
+// public class Debug {
+//
+//   public static void main(String[] args) {
+//
+//     System.out.println("       1");
+//     System.out.println("     2 3");
+//     System.out.println("   4 5 6");
+//     System.out.println("7 8 9 10")
+//
+//   }
+//
+// }
+
+
+public class Debug {
+
+  public static void main(String[] args) {
+
+    System.out.println("       1");
+    System.out.println("     2 3");
+    System.out.println("   4 5 6");
+    System.out.println("7 8 9 10");
+
+  }
+
+}
