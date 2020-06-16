28d36897c110423e2d48aa450809e37e2fe67871
Shades <shades4355@gmail.com>
2020-06-16 10:53:54 -0400

    Java - Logic Errors

---
+++ Java/13-Bugs/Logic_Errors.java
@@ -0,0 +1,31 @@
+// public class Debug {
+//
+//   public static void main(String[] args) {
+//
+//     int steps = 10;
+//
+//     for (int i = 0; i <= steps; i++) {
+//
+//       System.out.println("Step #" + i); //prints 0-10
+//
+//     }
+//
+//   }
+//
+// }
+
+public class Debug {
+
+  public static void main(String[] args) {
+
+    int steps = 10;
+
+    for (int i = 1; i <= steps; i++) {
+
+      System.out.println("Step #" + i); //prints 1-10
+
+    }
+
+  }
+
+}
